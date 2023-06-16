caaf6ec5a88e23e9b6d933817b41ed0a46ab063c
Kirill Kostakov <k.kostakov2002@gmail.com>
2023-06-16 17:24:10 +0200

    Add reviews to admin panel

--- cinema_backend/admin.py
+++ cinema_backend/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Movie, Hall, MovieSession, Genre
+from .models import Movie, Hall, MovieSession, Genre, Review
 
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(Hall)
 admin.site.register(MovieSession)
 admin.site.register(Genre)
+admin.site.register(Review)
